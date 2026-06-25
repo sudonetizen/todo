f92a5843dee4260f0f7a5f22c1ccef2b0ca9b5c9
sudonetizen <sardortolibjonov1039@gmail.com>
2026-06-26 02:35:47 +0500

    add TaskForm component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,30 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import TaskForm from './components/TaskForm.vue';
+
+const message = ref("Tasks App");
+
+function addTask(newTask: string) {
+    console.log(newTask);
+}
+
+</script>
+
 <template>
-    <h1>Hello World</h1>
+    <main>
+        <h1>{{ message }}</h1>
+        <TaskForm @add-task="addTask" />
+    </main>
 </template>
 
+<style>
+main {
+    max-width: 800px;
+    margin: 1rem auto;
+}
+
+.button-container {
+    display: flex;
+    justify-content: end;
+}
+</style>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const emit = defineEmits<{
+    addTask: [newTask: string]
+}>();
+
+const newTask = ref("");
+
+function formSubmitted() {
+    emit("addTask", newTask.value);
+}
+</script>
+
+<template>
+    <form @submit.prevent="formSubmitted">
+        <label>
+            New Task
+            <input v-model="newTask" name="newTask">
+        </label>
+        <div class="button-container">
+            <button>Add</button> 
+        </div>
+    </form>
+</template>
