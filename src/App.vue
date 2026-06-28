419b65e35f881eb1ff772c2f31767407768b3faf
sudonetizen <sardortolibjonov1039@gmail.com>
2026-06-28 21:03:23 +0500

    add task list component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import TaskForm from './components/TaskForm.vue';
+import TaskList from './components/TaskList.vue';
 import type { Task } from './types';
 
 const message = ref("Tasks App");
-const tasks = ref<Task>([]);
+const tasks = ref<Task[]>([]);
+const totalDone = computed(() => tasks.value.reduce((total, task) => task.done ? total + 1 : total, 0));
 
 function addTask(newTask: string) {
     tasks.value.push(
@@ -12,17 +14,26 @@ function addTask(newTask: string) {
     );
 }
 
+function toggleDone(id: string) {
+    const task = tasks.value.find((task) => task.id === id);
+    if (task) {
+        task.done = !task.done;
+    }
+}
+
 </script>
+
 
 <template>
     <main>
         <h1>{{ message }}</h1>
         <TaskForm @add-task="addTask" />
-        <article v-for="task in tasks" :key="task.id">
-            {{ task.title }}
-        </article>
+        <h3 v-if="!tasks.length">Add a task to get started!</h3>
+        <h3 v-else>{{ totalDone }} / {{ tasks.length }} tasks completed</h3>
+        <TaskList :tasks @toggle-done="toggleDone" />
     </main>
 </template>
+
 
 <style>
 main {
@@ -35,3 +46,5 @@ main {
     justify-content: end;
 }
 </style>
+
+
---
+++ src/components/TaskList.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import type { Task } from '../types';
+
+const props = defineProps<{
+    tasks: Task[]
+}>();
+
+const emits = defineEmits<{
+    toggleDone: [id: string]
+}>();
+
+</script>
+
+<template>
+    <div class="task-list">
+        <article v-for="task in tasks" :key="task.id">
+            <label>
+                <input
+                    @input="emits('toggleDone', task.id)"
+                    :checked="task.done"
+                    type="checkbox"
+                >
+                <span :class="{ done: task.done }">{{ task.title }}</span>
+            </label>
+        </article>
+    </div>
+</template>
+
+
+<style>
+.task-list {
+    margin-top: 1rem;
+}
+
+.done {
+    text-decoration: line-through;
+}
+</style>
